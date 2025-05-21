afe99f9978f50c483c06d37f51d847e381d5d174
auliarahmadani06 <129029899+auliarahmadani06@users.noreply.github.com>
2025-05-21 22:53:19 +0700

    liaa

--- tubes.c
+++ tubes.c
@@ -5,13 +5,10 @@ int main () {
     printf("bagus rahidya");
     printf("woilah coke");
     printf("anjay mabar ddsssd");
-<<<<<<< HEAD
     printf("liasyantikmaniezz");
-=======
     printf("misalnya");
     printf("ini ica");
     printf("adawdafafa");
->>>>>>> e97cf88525e8ab66441296b917fed797857de826
 return 0;
 
 }
